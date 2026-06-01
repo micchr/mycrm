3d270e6136f7a72b9c1926d1a0998e3b1730a8a9
APEXDOM\c.michel <c.michel@apexenergies.fr>
2026-06-01 15:27:57 +0200

    render config fix

--- mycrm/settings.py
+++ mycrm/settings.py
@@ -22,12 +22,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-9qwj&fkyrs4#lu_mcgrk29h-1$)@v^b+^0%apc47)x&a*0y+wi'
+SECRET_KEY = os.environ.get("SECRET_KEY", "unsafe-key")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
